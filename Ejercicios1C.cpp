586790ca87abe22d9b1f7f700c450123fb292f7e
talifali <tali.falicoff@hospitalitaliano.org.ar>
2025-05-20 19:42:37 -0300

    Add files via upload

---
+++ Ejercicios1C.cpp
@@ -0,0 +1,124 @@
+// Ejercicios1C.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	//EJERCICIO1
+	/* 
+	* float A,B,C,D,E,F;
+
+	A=B=C=D=E=F = 0;
+
+	cout << "Ingrese el primer digito";
+	cin >> A;
+
+	cout << "Ingrese el segundo digito";
+	cin >> B;
+
+	C = A + B; 
+	cout << "La suma de los digitos es "<< C;
+
+	D = A - B; 
+	cout << "La resta de los digitos es "<< D; 
+
+	E = A * B; 
+	cout << "El producto es " << E;
+
+	F = A / B; 
+	cout << "el cociente es " << F; 
+	*/
+
+	//EJERCICIO2
+	/*
+	cout << "Números entre 1 y 4" << 1 << 2 << 3 << 4;
+
+	cout << "Números entre 1 y 4" << 1 << 2 << 3 << 4; 
+
+	cout << "Primer numero entre 1 y 4" << 1<< endl; 
+	cout << "Segundo número entre 1 y 4" << 2<< endl; 
+	cout << "Tercer número entre 1 y 4" << 3<< endl; 
+	cout << "Último número entre 1 y 4" << 4<< endl; 
+	*/ 
+
+	//EJERCICIO3
+	/* 
+	float A,B,C,S,P,M,N,L; 
+	A = B = C = S = P = M = N = L = 0; 
+
+	cout << "Ingrese el primer número" <<A; 
+	cin >> A; 
+
+	cout << "Ingrese el segundo número" <<B;
+	cin >> B;
+
+	cout << "Ingrese el último número" <<C; 
+	cin >> C; 
+	
+	S = A + B + C;
+	cout << "La suma es"<<S<<endl;
+
+	L = A * B * C;
+	cout << "el producto es" << L << endl;
+
+	P = (A + B + C) / 3; 
+	cout << "El promedio es" << P << endl;
+
+	if (A > B && A > C) {
+		M = A;
+			cout << "El mayor es " << M << endl;
+	}
+	if (B > A && B > C) {
+		M = B;
+			cout << "El mayor es " << M << endl;
+	}
+	if (C > A && C > B) {
+		M = C;
+			cout << "El mayor es " << M << endl;
+	}
+
+	if (A < B && A < C) {
+		N = A;
+			cout << "El menor es " << N << endl;
+	}
+	if (B < A && B < C) {
+		N = B;
+			cout << "El menor es " << N << endl;
+	}
+	if (C < A && C < B) {
+		N = C;
+			cout << "El menor es " << N << endl;
+	}
+	*/
+
+	//EJERCICIO4
+	/* 
+	float R,D,C,PI,A; 
+	R = D = C = A = 0; 
+	PI = 3.1416; 
+
+	cout << "Ingrese el radio" <<R; 
+	cin >> R;
+
+	D = 2 * R; 
+	cout << "El diametro es " << D << endl; 
+
+	C = 2 * R * PI; 
+	cout << "La circunferencia es de " << C << endl; 
+
+	A = PI * (R * R);
+		cout << "El area es de " << A << endl;
+	*/
+//EJERCICIO5 
+float N, C, B; 
+cin>>"el cuadrdado de 0 es"
+cin>>"el cubo de 0 es"
+
+cin>>""
+
+	
+
+
+
+}
